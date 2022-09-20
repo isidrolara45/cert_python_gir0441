7c317c9219de2673d49cf61ad77462580830f784
isidrolara45 <isidrolara232@gmail.com>
2022-09-20 09:20:45 -0500

    laboratorio 2 1 1 18

---
+++ modulo2/lab2_1_1_18.py
@@ -0,0 +1,7 @@
+'''
+Descripcion: Programa muy facil de la funcion Print
+Autor: Isidro Lara López
+Fecha: 20 sep 2022
+'''
+print("Fundamentos","Programación","en", sep="***", end="...")
+print("Python")
