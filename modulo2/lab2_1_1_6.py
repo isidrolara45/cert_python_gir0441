61c847d5e0e8248b6542d662780f0dfcdfebc8e4
isidrolara45 <isidrolara232@gmail.com>
2022-09-20 08:41:30 -0500

    laboratorio 2 1 1 6

---
+++ modulo2/lab2_1_1_6.py
@@ -0,0 +1,8 @@
+'''
+Descripcion: Programa muy facil de la funcion Print
+Autor: Isidro Lara López
+Fecha: 20 sep 2022
+'''
+print("Hola Mundo!!!")
+print("Soy Isidro!!!")
+print("Bienvenido a Python")
